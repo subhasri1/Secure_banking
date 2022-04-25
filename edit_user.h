b7dc0b6c0e81e0ac9d9a882c94a908f021ab8b20
francis-rf <rfrancis789@gmail.com>
2022-04-25 11:19:31 +0530

    3files added

---
+++ edit_user.h
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+class edit_user
+{
+	
+	string user_name;
+	string gender;
+	long mob;
+	string type;
+public:
+	edit_user(string name,string gender,long mobno,string type);
+	void show();
+};
